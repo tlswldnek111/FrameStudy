3a050715cc09f6cac54692dac68db472ed3ac26e
Jiwoo Shin <1wo0ow1_jw@naver.com>
2022-05-27 09:30:27 +0900

    embeded tomcat

--- day02/src/main/java/com/bit/day02/MyTomcat.java
+++ day02/src/main/java/com/bit/day02/MyTomcat.java
@@ -1,26 +1,32 @@
 package com.bit.day02;
 
-//import javax.servlet.http.HttpServlet;
-//
-//import org.apache.catalina.LifecycleException;
-//import org.apache.catalina.Server;
-//import org.apache.catalina.startup.Tomcat;
+import java.io.File;
+
+import javax.servlet.http.HttpServlet;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Server;
+import org.apache.catalina.startup.Tomcat;
 
 
 public class MyTomcat {
 
 	public static void main(String[] args) {
-//		Tomcat serve=new Tomcat();
-//		serve.setPort(8080);
-//		try {
-//			serve.addWebapp("/","C:\\framework\\day02\\src\\main\\webapp");
-//			serve.addServlet("", "/ex01",(HttpServlet)Class.forName("com.bit.controller.Ex01Controller").newInstance());
-//			serve.start();
-//			Server server = serve.getServer();
-//			server.await();
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
+		Tomcat serve=new Tomcat();
+		serve.setPort(8080);
+		try {
+			Context cont=serve.addContext("/", new File(".").getAbsolutePath());
+			serve.addWebapp("/", new File(".").getAbsolutePath());
+			serve.addServlet(cont, "/ex01","com.bit.controller.Ex01Controller");
+			cont.addServletMapping("/ex01", "ex01");
+		
+		serve.start();
+			Server server = serve.getServer();
+			server.await();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
