c682df752110790a8a8b8dc20ae321c9f3393f53
Jiwoo Shin <1wo0ow1_jw@naver.com>
2022-05-30 10:57:34 +0900

    10am EmpDao, EmpVo, index

--- day05/src/main/java/com/bit/framework/DispatcherServlet.java
+++ day05/src/main/java/com/bit/framework/DispatcherServlet.java
@@ -1,6 +1,8 @@
 package com.bit.framework;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,9 +11,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.bit.emp.controller.IndexController;
+import com.bit.emp.controller.ListController;
 
 
 public class DispatcherServlet extends HttpServlet {
+	Map<String, BitController> cmap=new HashMap<>();
+	
+	@Override
+	public void init() throws ServletException {
+		cmap.put("/index.bit", new IndexController());
+		cmap.put("/emp/index.bit", new ListController());
+	}
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		doDo(req,resp);
@@ -25,14 +35,19 @@ public class DispatcherServlet extends HttpServlet {
 	public void doDo(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("dispatcher starttttttttt");
 		
+		//현재 사이트의 주소를 받아온다.
+		String url=req.getRequestURI().substring(req.getContextPath().length());
+		
 		BitController controller=null;//인터페이스 
-		controller=new IndexController();
+		controller=cmap.get(url);
 		String viewName="";
 		try {
 			viewName=controller.execute(req, resp);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		//redirect, forward 하는 url로 이동시키기
 		SimpleViewResolver resolver=new SimpleViewResolver();
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
