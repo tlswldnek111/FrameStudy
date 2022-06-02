cfafaf74d7ae43cefac90101f3cd73c9f3137cd6
Jiwoo Shin <1wo0ow1_jw@naver.com>
2022-06-02 09:59:40 +0900

    6/2 9am applicationContext, property, constructor-arg로 객체생성, getter setter

---
+++ sts02/src/main/java/com/bit/emp/App.java
@@ -0,0 +1,19 @@
+package com.bit.emp;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import java.sql.Date;
+import com.bit.emp.model.EmpVo;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext ac=null;
+		ac=new ClassPathXmlApplicationContext("/applicationContext.xml");
+		EmpVo bean=(EmpVo)ac.getBean("bean");//applicationContext에 있는 bean값을 여기로 가져온다.
+		System.out.println(bean.toString());
+		System.out.println(bean.getArr1());
+		
+	}
+
+}
