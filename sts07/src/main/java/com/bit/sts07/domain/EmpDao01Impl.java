437bfab39a5c9870880f6f8b80514ab62804c33a
Jiwoo Shin <1wo0ow1_jw@naver.com>
2022-06-10 11:09:33 +0900

    6/10 userinfo.properties 에서 root-context-> test

---
+++ sts07/src/main/java/com/bit/sts07/domain/EmpDao01Impl.java
@@ -0,0 +1,66 @@
+package com.bit.sts07.domain;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class EmpDao01Impl implements EmpDao {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+	
+	RowMapper<EmpVo> mapper=new RowMapper<EmpVo>() {
+
+		@Override
+		public EmpVo mapRow(ResultSet rs, int rowNum) throws SQLException {
+			
+			return new EmpVo(rs.getInt("empno"),rs.getInt("sal"),rs.getString("ename"),rs.getString("job"),rs.getTimestamp("hiredate"));
+		}
+		
+	};
+	
+	
+	@Override
+	public List<EmpVo> findAll() throws SQLException {
+		
+		
+		return null;
+	}
+
+	@Override
+	public EmpVo findOne(int idx) throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertOne(EmpVo bean) throws SQLException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int updateOne(EmpVo bean) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int deleteOne(int idx) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int allSize() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ sts07/src/test/java/com/bit/sts07/domain/EmpDaoTest.java
@@ -0,0 +1,71 @@
+package com.bit.sts07.domain;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class EmpDaoTest {
+	static EmpDao empDao;
+
+	@BeforeTestClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@Test
+	   public void testDataSource() throws BeansException, SQLException {
+	      ApplicationContext ac = null;
+	      ac = new GenericXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/root-context.xml");
+	      ac.getBean(DataSource.class).getConnection().close();
+	      
+	   }
+	
+	@Test
+	public void testFindAll() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindOne() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsertOne() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateOne() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeleteOne() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAllSize() {
+		//fail("Not yet implemented");
+	}
+
+}
