9444536435bafb1a3ac32b01e3eae7d3a361c4b8
Jiwoo Shin <1wo0ow1_jw@naver.com>
2022-06-14 17:58:42 +0900

    html, jsp 파일 사용해보기 . . .

--- boot01/src/main/java/com/bit/sts21/controller/MainController.java
+++ boot01/src/main/java/com/bit/sts21/controller/MainController.java
@@ -1,13 +1,14 @@
 package com.bit.sts21.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
 	
-	@GetMapping("/")
+	@GetMapping("/ex01")
 	public String welcome() {
-		return "welcome";
+		return "ex01";
 	}
 }
