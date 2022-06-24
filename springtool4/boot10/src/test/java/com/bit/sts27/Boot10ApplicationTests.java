56fbbfbe75d29bb6bdb3252f15574563c9b5744f
Jiwoo Shin <1wo0ow1_jw@naver.com>
2022-06-24 17:40:24 +0900

    stomp

---
+++ springtool4/boot10/src/test/java/com/bit/sts27/Boot10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.sts27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
